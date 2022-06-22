a760d1ce9ee1ea42c4ba6c0a441cfa6430d09e7d
Renato Moschetta <renatorock3@hotmail.com>
2022-06-21 22:00:47 -0300

    final entrega

--- src/pages/TableView/index.tsx
+++ src/pages/TableView/index.tsx
@@ -5,69 +5,85 @@ import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import "./styles.css";
+import api from "../../services/api";
 
 // Generate Order Data
-function createData(
-  id: number,
-  date: string,
-  name: string,
-  shipTo: string,
-  paymentMethod: string,
-  amount: number
-) {
-  return { id, date, name, shipTo, paymentMethod, amount };
+interface IExtratoAPI {
+  id: string;
+  data: string;
+  nota_FISCAL: string;
+  entrada_SAIDA: string;
+  valor: number;
+  produto: string;
+  quantidade: string;
+  fornecedor: string;
+  deposito: string;
 }
 
-const rows = [
-  createData(
-    0,
-    "16 Mar, 2019",
-    "Elvis Presley",
-    "Tupelo, MS",
-    "VISA ⠀•••• 3719",
-    312.44
-  ),
-  createData(
-    1,
-    "16 Mar, 2019",
-    "Paul McCartney",
-    "London, UK",
-    "VISA ⠀•••• 2574",
-    866.99
-  ),
-  createData(
-    2,
-    "16 Mar, 2019",
-    "Tom Scholz",
-    "Boston, MA",
-    "MC ⠀•••• 1253",
-    100.81
-  ),
-  createData(
-    3,
-    "16 Mar, 2019",
-    "Michael Jackson",
-    "Gary, IN",
-    "AMEX ⠀•••• 2000",
-    654.39
-  ),
-  createData(
-    4,
-    "15 Mar, 2019",
-    "Bruce Springsteen",
-    "Long Branch, NJ",
-    "VISA ⠀•••• 5919",
-    212.79
-  ),
-];
+interface IReposicaoProdAPI {
+  id_REP: string;
+  nom_DEPOS: string;
+  nom_PROD: string;
+  num_QTD_ESTOQUE: number;
+  num_QTD_REPOS: number;
+}
+
+interface IReposicaoFornecAPI {
+  id: string;
+  nom_DEPOS: string;
+  nom_FORNEC: string;
+  nom_PROD: string;
+  num_QTD_ESTOQUE: number;
+  num_QTD_REPOS: number;
+  num_CUSTO_UN_PROD: number;
+  dte: Date;
+}
+
+interface IPosicaoAPI {
+  id: number;
+  produto: string;
+  deposito: string;
+  saldo: number;
+}
 
 export default function TableView() {
+  const [extratoRow, setExtratoRow] = React.useState<IExtratoAPI[]>([]);
+  const [reposicaoProdRow, setReposicaoProdRow] = React.useState<
+    IReposicaoProdAPI[]
+  >([]);
+  const [reposicaoFornec, setReposicaoFornec] = React.useState<
+    IReposicaoFornecAPI[]
+  >([]);
+  const [posicao, setPosicao] = React.useState<IPosicaoAPI[]>([]);
+
+  React.useEffect(() => {
+    api.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${localStorage.getItem("@token")}`;
+
+    api.get("/relatorio/extratomovimenta").then((res) => {
+      setExtratoRow(res.data);
+    });
+
+    api.get("/relatorio/ponto-reposicao-produto").then((res) => {
+      setReposicaoProdRow(res.data);
+    });
+
+    api.get("/relatorio/ponto-reposicao-fornecedor").then((res) => {
+      setReposicaoFornec(res.data);
+    });
+
+    api.get("/relatorio/posicao-estoque-deposito").then((res) => {
+      setPosicao(res.data);
+    });
+  }, []);
+
   return (
     <React.Fragment>
       <div className="table-view">
         <div>
           <h2>Extrato de Movimentação do Estoque</h2>
-          <Table size="medium">
+          <Table size="small">
             <TableHead>
               <TableRow>
                 <TableCell>Data</TableCell>
@@ -81,16 +97,16 @@ export default function TableView() {
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row) => (
+              {extratoRow.map((row) => (
                 <TableRow key={row.id}>
-                  <TableCell>{row.date}</TableCell>
-                  <TableCell>{row.name}</TableCell>
-                  <TableCell>{row.shipTo}</TableCell>
-                  <TableCell>{row.shipTo}</TableCell>
-                  <TableCell>{row.shipTo}</TableCell>
-                  <TableCell>{row.shipTo}</TableCell>
-                  <TableCell>{row.paymentMethod}</TableCell>
-                  <TableCell>{`$${row.amount}`}</TableCell>
+                  <TableCell>{row.data}</TableCell>
+                  <TableCell>{row.nota_FISCAL}</TableCell>
+                  <TableCell>{row.entrada_SAIDA}</TableCell>
+                  <TableCell>{row.valor}</TableCell>
+                  <TableCell>{row.produto}</TableCell>
+                  <TableCell>{row.quantidade}</TableCell>
+                  <TableCell>{row.fornecedor}</TableCell>
+                  <TableCell>{row.deposito}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
@@ -100,22 +116,22 @@ export default function TableView() {
       <div className="table-view">
         <div>
           <h2>Produtos em ponto de Reposição</h2>
-          <Table size="medium">
+          <Table size="small">
             <TableHead>
               <TableRow>
                 <TableCell>Produto</TableCell>
-                <TableCell>Saldo</TableCell>
-                <TableCell>Ponto de Reposição</TableCell>
                 <TableCell>Deposito</TableCell>
+                <TableCell>Quantidade Estoque</TableCell>
+                <TableCell>Quanatidade Reposição</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row) => (
-                <TableRow key={row.id}>
-                  <TableCell>{row.date}</TableCell>
-                  <TableCell>{row.name}</TableCell>
-                  <TableCell>{row.shipTo}</TableCell>
-                  <TableCell>{row.shipTo}</TableCell>
+              {reposicaoProdRow.map((row) => (
+                <TableRow key={row.id_REP}>
+                  <TableCell>{row.nom_PROD}</TableCell>
+                  <TableCell>{row.nom_DEPOS}</TableCell>
+                  <TableCell>{row.num_QTD_ESTOQUE}</TableCell>
+                  <TableCell>{row.num_QTD_REPOS}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
@@ -127,24 +143,28 @@ export default function TableView() {
           <h2>
             Produtos em ponto de reposição x Fornecedores para cotação de compra
           </h2>
-          <Table size="medium">
+          <Table size="small">
             <TableHead>
               <TableRow>
-                <TableCell>Produto</TableCell>
-                <TableCell>Saldo</TableCell>
-                <TableCell>Ponto de Reposição</TableCell>
                 <TableCell>Deposito</TableCell>
-                <TableCell>Preço de Compra</TableCell>
+                <TableCell>Nome Fornecedor</TableCell>
+                <TableCell>Quantidade Reposição</TableCell>
+                <TableCell>Custo Unitário de Produto</TableCell>
+                <TableCell>Data do Cadastro</TableCell>
+                <TableCell>Nome Produto</TableCell>
+                <TableCell>Quantidade Estoque</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row) => (
+              {reposicaoFornec.map((row) => (
                 <TableRow key={row.id}>
-                  <TableCell>{row.date}</TableCell>
-                  <TableCell>{row.name}</TableCell>
-                  <TableCell>{row.shipTo}</TableCell>
-                  <TableCell>{row.shipTo}</TableCell>
-                  <TableCell>{row.shipTo}</TableCell>
+                  <TableCell>{row.nom_DEPOS}</TableCell>
+                  <TableCell>{row.nom_FORNEC}</TableCell>
+                  <TableCell>{row.num_QTD_REPOS}</TableCell>
+                  <TableCell>{row.num_CUSTO_UN_PROD}</TableCell>
+                  <TableCell>{String(row.dte)}</TableCell>
+                  <TableCell>{row.nom_PROD}</TableCell>
+                  <TableCell>{row.num_QTD_ESTOQUE}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
@@ -163,11 +183,11 @@ export default function TableView() {
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row) => (
+              {posicao.map((row) => (
                 <TableRow key={row.id}>
-                  <TableCell>{row.date}</TableCell>
-                  <TableCell>{row.name}</TableCell>
-                  <TableCell>{row.shipTo}</TableCell>
+                  <TableCell>{row.produto}</TableCell>
+                  <TableCell>{row.saldo}</TableCell>
+                  <TableCell>{row.deposito}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
