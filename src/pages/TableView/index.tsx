187cc17d74e16051eafd844c1140d3398c580899
Renato Moschetta de Souza <renatorock3@hotmail.com>
2022-06-22 21:47:36 -0300

    Update index.tsx

--- src/pages/TableView/index.tsx
+++ src/pages/TableView/index.tsx
@@ -6,6 +6,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import "./styles.css";
 import api from "../../services/api";
+import Moment from 'moment';
 
 // Generate Order Data
 interface IExtratoAPI {
